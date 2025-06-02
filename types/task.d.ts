db659b3752d1c391130be65bff195392a741bdaa
Taofiq <animashauntaofiq@gmail.com>
2025-06-02 23:36:06 +0100

    Add TaskData interface definition for task structure

---
+++ types/task.d.ts
@@ -0,0 +1,11 @@
+import { Timestamp } from "firebase/firestore";
+
+interface TaskData {
+  id?: string;
+  name: string;
+  description?: string;
+  subtasks: { name: string; isCompleted: boolean }[];
+  boardId: string;
+  status: string;
+  createdAt: Timestamp;
+}
