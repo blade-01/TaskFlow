5eb9bfdc41a126ddc3f2edbc44728eeb9a4bc831
Taofiq <animashauntaofiq@gmail.com>
2025-06-02 23:34:47 +0100

    Remove DashboardLayout from Board route in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "@theme-toggles/react/css/Classic.css";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
 import { PrimeReactProvider } from "primereact/api";
 import Auth from "./pages/Auth";
-import DashboardLayout from "./layouts/DashboardLayout";
 import { SidebarProvider } from "./context/SidebarContext";
 import Board from "./pages/Board";
 import ProtectedRoute from "./components/Navigation/ProtectedRoutes";
@@ -18,9 +17,7 @@ function App() {
               path="/board/:id"
               element={
                 <ProtectedRoute>
-                  <DashboardLayout>
-                    <Board />
-                  </DashboardLayout>
+                  <Board />
                 </ProtectedRoute>
               }
             />
