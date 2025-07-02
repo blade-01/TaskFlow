b1a37631771a3afd3f9b196bbed263be9a2eb354
Taofiq <animashauntaofiq@gmail.com>
2025-07-02 10:16:09 +0100

    Refactor Delete component by removing unnecessary wrapper div around CenterModal

--- src/components/Ui/Modal/Delete.tsx
+++ src/components/Ui/Modal/Delete.tsx
@@ -15,37 +15,35 @@ export default function Delete({
   handleDelete: () => void;
 }) {
   return (
-    <div>
-      <CenterModal
-        title={title}
-        visible={visible}
-        setVisible={(event) => setVisible(event)}
-      >
-        <div className="flex flex-col gap-4">
-          <h1 className="text-lg font-semibold">
-            Are you sure you want to delete this board?
-          </h1>
-          <p className="text-sm text-main-text">
-            This action cannot be undone. All tasks and data will be permanently
-            deleted.
-          </p>
-          <div className="flex justify-end gap-2">
-            <Btn
-              onClick={() => setVisible(false)}
-              outerClass="btn btn-sm btn-default"
-            >
-              Cancel
-            </Btn>
-            <Btn
-              onClick={() => handleDelete()}
-              isLoading={isLoading}
-              outerClass="btn btn-sm btn-danger"
-            >
-              Delete
-            </Btn>
-          </div>
+    <CenterModal
+      title={title}
+      visible={visible}
+      setVisible={(event) => setVisible(event)}
+    >
+      <div className="flex flex-col gap-4">
+        <h1 className="text-lg font-semibold">
+          Are you sure you want to delete this board?
+        </h1>
+        <p className="text-sm text-main-text">
+          This action cannot be undone. All tasks and data will be permanently
+          deleted.
+        </p>
+        <div className="flex justify-end gap-2">
+          <Btn
+            onClick={() => setVisible(false)}
+            outerClass="btn btn-sm btn-default"
+          >
+            Cancel
+          </Btn>
+          <Btn
+            onClick={() => handleDelete()}
+            isLoading={isLoading}
+            outerClass="btn btn-sm btn-danger"
+          >
+            Delete
+          </Btn>
         </div>
-      </CenterModal>
-    </div>
+      </div>
+    </CenterModal>
   );
 }
